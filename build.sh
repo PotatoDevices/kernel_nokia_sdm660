4aa66c54c286e344c9d3e917250c6290b8dfef2f
SahilSonar <sss.sonar2003@gmail.com>
2021-05-25 11:00:24 +0200

    build.sh: Simplify build script logic

--- build.sh
+++ build.sh
@@ -10,94 +10,86 @@ nocol='\033[0m'
 
 # Kernel details
 KERNEL_NAME="Quindecim"
-VERSION="r1.0"
 DATE=$(date +"%d-%m-%Y-%I-%M")
 DEVICE="DRG"
-FINAL_ZIP=$KERNEL_NAME-$VERSION-$DATE-$DEVICE.zip
+FINAL_ZIP=$KERNEL_NAME-$DATE-$DEVICE.zip
 defconfig=nokia_defconfig
 
 # Dirs
 BASE_DIR=`pwd`/../
 KERNEL_DIR=$BASE_DIR/sdm660
 ANYKERNEL_DIR=$KERNEL_DIR/AnyKernel3
-KERNEL_IMG=$KERNEL_DIR/output/arch/arm64/boot/Image.gz-dtb
+KERNEL_IMG=$KERNEL_DIR/outdir/arch/arm64/boot/Image.gz-dtb
 UPLOAD_DIR=$BASE_DIR/$DEVICE
 
 # Export
-export PATH="$BASE_DIR/proton-clang/bin:$PATH"
+export PATH="$BASE_DIR/clang/bin:$BASE_DIR/arm64-gcc/bin:$BASE_DIR/arm-gcc/bin:${PATH}"
 export ARCH=arm64
-export CROSS_COMPILE=aarch64-linux-gnu-
-export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
 
-## Functions ##
+## Funtions
+# Clean Compile
+function clean_compile() {
+echo "---------------------------------------"
+make O=outdir clean
+echo "---------------------------------------"
+make O=outdir mrproper
+echo "---------------------------------------"
+make O=outdir ARCH=arm64 $defconfig
 
-# Make kernel
-function make_kernel() {
-  echo -e "$cyan***********************************************"
-  echo -e "          Initializing defconfig          "
-  echo -e "***********************************************$nocol"
-  make $defconfig CC=clang O=output/
-  echo -e "$cyan***********************************************"
-  echo -e "             Building kernel          "
-  echo -e "***********************************************$nocol"
-  make -j$(nproc --all) CC=clang O=output/
+make -j$(nproc --all) O=outdir \
+                      ARCH=arm64 \
+                      CC=clang \
+                      CLANG_TRIPLE=aarch64-linux-gnu- \
+                      CROSS_COMPILE=aarch64-linux-android- \
+                      CROSS_COMPILE_ARM32=arm-linux-androideabi-
   if ! [ -a $KERNEL_IMG ];
   then
     echo -e "$red Kernel Compilation failed! Fix the errors! $nocol"
     exit 1
   fi
+echo "---------------------------------------"
 }
 
-# Making zip
+# Dity Compile
+function dirty_compile() {
+echo "---------------------------------------"
+make O=outdir ARCH=arm64 $defconfig
+
+make -j$(nproc --all) O=outdir \
+                      ARCH=arm64 \
+                      CC=clang \
+                      CLANG_TRIPLE=aarch64-linux-gnu- \
+                      CROSS_COMPILE=aarch64-linux-android- \
+                      CROSS_COMPILE_ARM32=arm-linux-androideabi-
+  if ! [ -a $KERNEL_IMG ];
+  then
+    echo -e "$red Kernel Compilation failed! Fix the errors! $nocol"
+    exit 1
+  fi
+echo "---------------------------------------"
+}
+
+# Regenerate defconfig
+function regen() {
+echo "---------------------------------------"
+make O=outdir clean
+echo "---------------------------------------"
+make O=outdir mrproper
+export ARCH=arm64
+echo "---------------------------------------"
+make $defconfig
+echo "Done!"
+echo "---------------------------------------"
+}
+
+# Zip Kernel
 function make_zip() {
 cp $KERNEL_IMG $ANYKERNEL_DIR
 mkdir -p $UPLOAD_DIR
 cd $ANYKERNEL_DIR
 zip -r9 UPDATE-AnyKernel2.zip * -x README UPDATE-AnyKernel2.zip
 mv $ANYKERNEL_DIR/UPDATE-AnyKernel2.zip $UPLOAD_DIR/$FINAL_ZIP
-}
-
-# Options
-function options() {
-echo -e "$cyan***********************************************"
-  echo "          Compiling Quindecim Kernel          "
-  echo -e "***********************************************$nocol"
-  echo -e " "
-  echo -e " Select one of the following types of build : "
-  echo -e " 1.Dirty"
-  echo -e " 2.Clean"
-  echo -n " Your choice : ? "
-  read ch
-
-  echo -e " Select if you want zip or just kernel : "
-  echo -e " 1.Get flashable zip"
-  echo -e " 2.Get kernel only"
-  echo -n " Your choice : ? "
-  read ziporkernel
-
-case $ch in
-  1) echo -e "$cyan***********************************************"
-     echo -e "          	Dirty          "
-     echo -e "***********************************************$nocol"
-     make_kernel ;;
-  2) echo -e "$cyan***********************************************"
-     echo -e "          	Clean          "
-     echo -e "***********************************************$nocol"
-     make clean CC=clang O=output/
-     make mrproper CC=clang O=output/
-     make_kernel ;;
-esac
-
-if [ "$ziporkernel" = "1" ]; then
-     echo -e "$cyan***********************************************"
-     echo -e "     Making flashable zip        "
-     echo -e "***********************************************$nocol"
-     make_zip
-else
-     echo -e "$cyan***********************************************"
-     echo -e "     Building Kernel only        "
-     echo -e "***********************************************$nocol"
-fi
+echo "---------------------------------------"
 }
 
 # Clean Up
@@ -105,7 +97,26 @@ function cleanup(){
 rm -rf $ANYKERNEL_DIR/Image.gz-dtb
 }
 
-options
+# Menu
+function menu() {
+echo "---------------------------------------"
+echo -e "1. Dirty"
+echo -e "2. Clean"
+echo -e "3. Regenerate Defconfig"
+echo -n "Choose :"
+read choose
+
+case $choose in
+ 1) dirty_compile
+    make_zip ;;
+ 2) clean_compile
+    make_zip ;;
+ 3) regen ;;
+esac
+}
+
+
+menu
 cleanup
 BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
